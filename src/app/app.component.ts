aa6672c4e2d9330564d7d23d69b4e3c5ebfff411
Ylwoi <balazs.horvath989@gmail.com>
2019-01-18 16:49:29 +0100

    Sort the parking locations by distance and mark the closest on the map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { Http } from '@angular/http';
 
+import Geo from 'geo-nearby';
+import sortByDistance from 'sort-by-distance';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,6 +16,7 @@ export class AppComponent {
   parkingLat: number;
   parkingLng: number;
   apiRoot: string = "https://kozutnhptest.azurewebsites.net";
+  parkingIcon: string = "https://maps.google.com/mapfiles/kml/shapes/parking_lot_maps.png";
   //parkingLocations: Object;
 
   constructor(private http: Http) { }
@@ -38,13 +42,44 @@ export class AppComponent {
   getParkingLocations() {
     let url = `${this.apiRoot}/api/Map/GetParkingStations`;
     this.http.get(url).subscribe(res => {
-      this.getNearestLocation(res.json())
+      const a = performance.now();
+      //this.getNearestLocation(res.json())
+      this.getNearestLocation2(res.json());
+      const b = performance.now();
+      console.log(('It took ' + (b - a) + ' ms.'));
     });
   }
 
-  getNearestLocation(parkingLocations: Object) {
-    console.log(parkingLocations)
-  }
+  // getNearestLocation(parkingLocations: Object) {
+  //   console.log(parkingLocations);
+  //   for (let index in parkingLocations) {
+  //     parkingLocations[index]["id"] = index;
+  //   }
+  //   const parkingLocsSet = Geo.createCompactSet(parkingLocations, { id: 'id', lat: 'latitude', lon: 'longitude' });
+  //   console.log(parkingLocsSet);
+  //   const geo = new Geo(parkingLocsSet);
+  //   console.log(geo.limit(1).nearBy(this.userLat, this.userLng, 10000));
+  //   const nearbyParkingId = geo.limit(1).nearBy(this.userLat, this.userLng, 10000)[0]['i'];
+  //   console.log(nearbyParkingId);
+  //   for (let index in parkingLocations) {
+  //     if (parkingLocations[index]['id'] == nearbyParkingId) {
+  //       this.parkingLat = parkingLocations[index]["latitude"];
+  //       this.parkingLng = parkingLocations[index]["longitude"];
+  //       break;
+  //     }
+  //   }
+  // }
 
+  getNearestLocation2(parkingLocations: Object) {
+    const opts = {
+      xName: 'latitude',
+      yName: 'longitude'
+    }
+    const origin = { latitude: this.userLat, longitude: this.userLng }
+
+    const sortedParkingLocations = sortByDistance(origin, parkingLocations, opts);
+    console.log(sortedParkingLocations);
+    this.parkingLat = sortedParkingLocations[0]['latitude'];
+    this.parkingLng = sortedParkingLocations[0]['longitude'];
+  }
 }
-//455
